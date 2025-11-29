033de0ed2d81f21cfab052225b270685205c0fed
Jeff A. <jepezi@gmail.com>
2025-11-29 16:34:19 +0700

    force dynamic sitemap

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { generateMileSitemap } from "@milejs/core/app";
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const result = await generateMileSitemap();
   return result;
